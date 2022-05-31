ea49048ad56332b6462e526a1ef2aeb157e74b5a
lazazael@gmail.com <git99hub>
2022-05-31 10:33:32 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v, err")
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Username = %s\n", username)
+	fmt.Fprintf(w, "Email = %s\n", email)
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/about" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "about page")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/about", aboutHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
